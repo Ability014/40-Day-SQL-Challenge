d11201c96e6967c6ee9eebefa9157fa3ab73fb37
Ability014 <54489393+Ability014@users.noreply.github.com>
2023-02-14 12:54:28 +0100

    Add files via upload

--- sql 40-day challenge solution.sql
+++ sql 40-day challenge solution.sql
@@ -92,7 +92,8 @@ employee_attendance (ID VARCHAR(10), EMPLOYEE_ID INT, TIME_IN DATETIME, TIME_OUT
 
 WITH EMPLOYEE_TIME_DIFF AS (
 SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, DATENAME(WEEKDAY, TIME_IN) WEEK_DAY,
-DATEDIFF(DAY, TIME_IN, LEAD(TIME_IN) OVER(PARTITION BY EMPLOYEE_ID, CONCAT(DATEPART(YEAR FROM TIME_IN), DATEPART(WEEK FROM TIME_IN)) ORDER BY TIME_IN)) DATE_LAG_DIFF
+DATEDIFF(DAY, TIME_IN, LEAD(TIME_IN) OVER(PARTITION BY EMPLOYEE_ID, 
+CONCAT(DATEPART(YEAR FROM TIME_IN), DATEPART(WEEK FROM TIME_IN)) ORDER BY TIME_IN)) DATE_LAG_DIFF
 FROM employee_attendance)
 SELECT DISTINCT EMPLOYEE_ID, FIRST_NAME, LAST_NAME
 FROM EMPLOYEE_TIME_DIFF
@@ -436,7 +437,8 @@ NB:
 trip_date: Time at which the trip was initiated
 request_time: Time at which passenger successfully requested a ride
 driver_arrived: Time the driver got to the pickup location
-cancelled_at: Time at which the passenger cancelled the ride started_at: Time at which the driver started the trip
+cancelled_at: Time at which the passenger cancelled the ride 
+started_at: Time at which the driver started the trip
 dropoff_at: Time the trip was completed
 */
 -- Waiting time should be the time it took for driver to arrive at pickup location after request time.
@@ -445,41 +447,311 @@ SELECT DATEDIFF(MINUTE, request_time, driver_arrived) WAITING_TIME,
 FROM rides;
 
 -- Q21.
+/*
+Use table from 20 to answer this metric: Number of successful/cancelled trips for each month as a pivot
+*/
+SELECT DATEPART(YEAR FROM trip_date) TRIP_YEAR, DATEPART(MONTH FROM trip_date) TRIP_MONTH,
+COUNT(CASE WHEN cancelled_at IS NULL THEN id END) SUCCESSFUL_TRIPS, 
+COUNT(CASE WHEN cancelled_at IS NOT NULL THEN id END) CANCELLED_TRIPS
+FROM rides
+GROUP BY DATEPART(YEAR FROM trip_date), DATEPART(MONTH FROM trip_date);
 
 -- Q22.
+/*
+Use table from 20 to answer this metric: Number of successful trips and average travel time per driver
+*/
+SELECT driver_id, COUNT(id) SUCCESSFUL_TRIPS, AVG(DATEDIFF(MINUTE, dropoff_at, started_at)) AVG_TRAVEL_TIME
+FROM rides
+WHERE cancelled_at IS NULL 
+GROUP BY driver_id;
 
 -- Q23.
+/*
+Use table from 20 to answer this metric: Top 10 drivers (percentage of successful trips compared to total ride requests).
+Only consider total trips per driver that exceeds the average number of trips for all drivers.
+*/
+WITH AGG_DRIVER_STAT AS (
+SELECT driver_id, COUNT(CASE WHEN cancelled_at IS NULL THEN id END) SUCCESSFUL_TRIPS, 
+COUNT(id) TOTAL_TRIPS
+FROM rides
+GROUP BY driver_id)
+SELECT TOP 10 *, SUCCESSFUL_TRIPS/TOTAL_TRIPS * 100 PERCENT_SUCCESSFUL_TRIPS
+FROM AGG_DRIVER_STAT
+WHERE TOTAL_TRIPS > (SELECT AVG(TOTAL_TRIPS) FROM AGG_DRIVER_STAT)
+ORDER BY PERCENT_SUCCESSFUL_TRIPS DESC;
 
 -- Q24.
+/*
+Use table from 20 to answer this metric: Waiting time between a passenger cancelled order and the next order request.
+*/
+WITH AGG_DRIVER_STAT AS (
+SELECT passenger_id, 
+	  (LEAD(trip_date) OVER(PARTITION BY passenger_id, CAST(trip_date AS DATE) ORDER BY trip_date)) next_order_time, cancelled_at
+FROM rides)
+SELECT *, DATEDIFF(mm, cancelled_at, next_order_time) waiting_time_before_next_req
+FROM AGG_DRIVER_STAT
+WHERE DATEDIFF(mm, cancelled_at, next_order_time) IS NOT NULL;
 
 -- Q25.
+/*
+A transportation business just lauched its mobile app for customers to order rides to 
+diffferent destinations. This application is been monitored for quality assurance purpose
+and logs of the ride orders are logged into ride_logs table. Highlighted below is the 
+structure of the table:
+
+ride_logs (log_id, trip_id, trip_date, passenger_id, passenger_cancelled, pickup, destination);
+
+The business needs to be aware based on historical log data available, is the distribution
+of monthly cancelled rides a decreasing sequence? Write a query to answer the business question.
+
+NB: passenger_cancelled is the time at which a passenger calcelled the order else NULL
+*/
+WITH MONTHLY_CANC_TRIPS AS (
+SELECT DATEPART(YEAR FROM trip_date) TRIP_YEAR, DATEPART(MONTH FROM trip_date) TRIP_MONTH, 
+COUNT(CASE WHEN passenger_cancelled IS NOT NULL THEN trip_id END) NO_CANCELED_TRIPS
+FROM ride_logs
+GROUP BY DATEPART(YEAR FROM trip_date), DATEPART(MONTH FROM trip_date)),
+Monthly_Diff_of_Canc_Trips As (
+SELECT *, CASE WHEN (LEAD(NO_CANCELED_TRIPS) OVER(ORDER BY TRIP_YEAR, TRIP_MONTH) - NO_CANCELED_TRIPS) > 0 THEN 1 ELSE 0 END As Monthly_Difference
+FROM MONTHLY_CANC_TRIPS)
+SELECT CASE WHEN SUM(Monthly_Difference) = 0 THEN TRUE ELSE FALSE END As DECREASING_SEQUENCE 
+FROM Monthly_Diff_of_Canc_Trips;
 
 -- Q26.
+/*
+A financial institution is planning a reward model for its customers that do not spend up to
+50% of their inflow in the previous month. Write an SQL query to identify such customers
+from the transaction table.
+
+Constraints: The customer credit transaction for the month must be at least 50,000.
+
+NB: A customer can have more than 1 account
+*/
+SELECT CUSTOMER_ID, SUM(CASE WHEN TRAN_TYPE = 'C' THEN AMOUNT END) INFLOW, SUM(CASE WHEN TRAN_TYPE = 'D' THEN AMOUNT END) OUTFLOW
+FROM TRANX_TABLE
+WHERE TRAN_DATE > CAST(dateadd(d, -1, dateadd(mm,datediff(m,0,getdate())-1,0)) AS DATE) AND TRAN_DATE <  CAST(dateadd(mm,datediff(m,0,getdate())-0,0) AS DATE)
+GROUP BY CUSTOMER_ID
+HAVING (SUM(CASE WHEN TRAN_TYPE = 'C' THEN AMOUNT END)/2) < SUM(CASE WHEN TRAN_TYPE = 'D' THEN AMOUNT END); 
 
 -- Q27.
+/*
+As an analyst for a sport company, you are requested to present the football data available
+in the database as: Form of last five matches of each football team in the football table
+as a pivot table.
+
+Form implies either W, D or L. Where W: win, D: Draw, L: Lose
+*/
+WITH ALL_FORM AS
+(
+SELECT [DATE], Home_Team Team, CASE WHEN Home_Score > Away_Score THEN 'W' 
+									WHEN Home_Score < Away_Score THEN 'L'
+									ELSE 'D' END FORM
+FROM matches
+WHERE [DATE] BETWEEN DATEADD(month, -90, GETDATE()) AND GETDATE() --USING LAST 90 DAYS AS A THRESHOLD
+UNION ALL
+SELECT [DATE], Away_Team Team, CASE WHEN Home_Score < Away_Score THEN 'W' 
+									WHEN Home_Score > Away_Score THEN 'L'
+									ELSE 'D' END FORM
+FROM matches
+WHERE [DATE] BETWEEN DATEADD(month, -90, GETDATE()) AND GETDATE()
+),
+RANK_FORM AS 
+(SELECT *, RANK() OVER(PARTITION BY Team ORDER BY [DATE] DESC) RNK
+FROM ALL_FORM)
+SELECT *
+FROM (SELECT Team, FORM, RNK 
+FROM RANK_FORM
+WHERE RNK <= 5) T1
+PIVOT (MAX(FORM) FOR RNK IN ([1],[2],[3],[4],[5])) PVT;
+
 
 -- Q28.
+/*A transportation business is interested in understanding its customers so as to provide a
+more personalized experience. The business needs to identify customers that ordered a return
+ride to their pick up location.
+
+A typical scenario will be used in this conext. We assume that Pickup_location will be same as Dropoff_location in a return ride scenario.
+
+trips(trip_id, passenger_id, driver_id, pickup_datetime, dropoff_datetime, Trip_distance, 
+pickup_location, Standard_rate, dropoff_location, Payment_type, Fare_amount, Tolls_amount, Total_amount)
+*/
+--Assumption: A ride is referred as return ride if the return ride was on the same day
+--This trip returns all return trips for eah passengers, and tells time out & time in of pickup location.
+WITH RANKED_TRIPS AS (
+SELECT trip_id, passenger_id, pickup_location, dropoff_location,
+FORMAT(pickup_datetime,'yyyy-MM-dd') pickup_date,
+FORMAT(dropoff_datetime,'yyyy-MM-dd') droppodff_date,
+pickup_datetime, dropoff_datetime,
+RANK() OVER(PARTITION BY passenger_id, FORMAT(pickup_datetime,'yyyy-MM-dd') ORDER BY pickup_datetime) pickup_rank,
+RANK() OVER(PARTITION BY passenger_id, FORMAT(pickup_datetime,'yyyy-MM-dd') ORDER BY dropoff_datetime) droppoff_rank
+FROM trips)
+SELECT RT1.trip_id, RT1.passenger_id, RT1.pickup_location, RT2.dropoff_location, RT1.pickup_datetime, RT2.dropoff_datetime
+FROM RANKED_TRIPS RT1
+JOIN RANKED_TRIPS RT2
+ON RT1.passenger_id = RT2.passenger_id AND RT1.pickup_date=RT2.pickup_date AND RT1.pickup_location=RT2.dropoff_location AND RT1.pickup_rank > RT2.droppoff_rank;
 
 -- Q29.
+/*
+You have been provided with employees table, and you are charged to create an Org chart with it.
+How do you write a query to identify the hierachy of employees from the employees table?
+
+The sample of employees data is attached to this post.
+*/
+WITH EMPLOYEE_HIERARCHY AS 
+(
+SELECT 1 As Hierarchy, *
+FROM EMPLOYEES
+WHERE MANAGER_ID IS NULL
+UNION ALL
+SELECT Hierarchy+1, M.*
+FROM EMPLOYEE_HIERARCHY E
+JOIN EMPLOYEES M
+ON E.EMPLOYEE_ID=M.MANAGER_ID
+)
+SELECT * FROM EMPLOYEE_HIERARCHY;
 
 -- Q30.
+/*
+Write a query to return employees with lowest salary in each department;
+*/
+WITH RANK_DEPT_SALARY AS
+(
+SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY, RANK() OVER(PARTITION BY DEPT_ID ORDER BY SALARY) RANKED_DEPT_SALARY
+FROM EMPLOYEES
+)
+SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY
+FROM RANK_DEPT_SALARY
+WHERE RANKED_DEPT_SALARY = 1;
 
+-- Bonus Questions
 -- Q31.
+/*
+Is there anything wrong with the SQL statement below?
+
+SELECT customer_id, sum(amount) revenue
+FROM customer_tranx
+GROUP BY customer_id
+WHERE sum(amount) > 20000;
+*/
+--1. WHERE clause should be used before a GROUP BY
+--2. Aggregate functions shouldn't be used in a WHERE clause
 
 -- Q32.
+/*
+Given that an employee table has details of about 50 employees with an ID ranging from 1 - 50, what is the appropriate operator in the query below to select details of all 
+employees whose salary is above all employees with an ID above 40?
+
+SELECT *
+
+FROM EMPLOYEES E1
+
+WHERE SALARY _____ (SELECT SALARY FROM EMPLOYEES E2 WHERE ID > 40);
+*/
+SELECT *
+FROM EMPLOYEES E1
+WHERE SALARY >ALL (SELECT SALARY FROM EMPLOYEES E2 WHERE ID > 40);
 
 -- Q33.
+/*
+Is this query valid? If yes, what is the behaviour?
+
+SELECT first_name, last_name, SALARY/(SELECT SUM(SALARY) FROM EMPLOYEES) 
+FROM EMPLOYEES
+WHERE SALARY/(SELECT SUM(SALARY) FROM EMPLOYEES) > 0.005;
+*/
+--Yes, the query is valid. It returns details of employees whose salary is above 0.5% of all employees salaries.
 
 -- Q34.
+/*
+Given a table cust_tranx with 1,000,000 records, what is the result/behaviour of the query below 
+and why?
+
+SELECT SUM(1)
+FROM cust_tranx;
+*/
+--The query returns 1,000,000. It sums 1 as a scalar value in 1 million places.
 
 -- Q35.
+/*
+What is the behavior of the UPDATE statement below?
+
+UPDATE EMPLOYEES
+SET SALARY = (CASE WHEN ID > 3 THEN SALARY*2 END);
+*/
+--The query doubles the salary of employees with ID greater than 3 while the other employee salaries is replaced with NULL
 
 -- Q36.
+/*
+What is the behavior of this query?
+
+SELECT MAX(CASE WHEN SALARY > 300000 THEN 1 ELSE 0 END)
+FROM EMPLOYEES;
+*/
+--This query is trying to check if there is any employee with salary above 300,000
 
 -- Q37.
+/*
+What is the result of the query below?
+
+SELECT NAME, SALARY
+FROM EMPLOYEES
+HAVING SALARY > 10000;
+*/
+--The query throws an error as the salary column is neither an aggregate or in a GROUP BY clause. A WHERE clause is appropriate in this case.
 
 -- Q38.
+/*
+Given table TRANX having 50000 records, what is the result of the query below?
+
+SELECT 2*COUNT(2), 2*SUM(2) FROM TRANX;
+*/
+--2*COUNT(2) = 2*50000 = 100000
+--2*SUM(2) = 2*50000*2 = 200000
 
 -- Q39.
+/*
+A phone call is considered an international call when the person calling is in a different country than the person receiving the call.
+
+What percentage of phone calls are international? Round the result to 1 decimal.
+
+Assumption:
+
+The caller_id in phone_info table refers to both the caller and receiver.
+
+phone_calls(caller_id, receiver_id, call_time)
+
+phone_info(caller_id, country_id, network, phone_no)
+*/
+SELECT 
+  ROUND(
+    100.0 * (COUNT(CASE  WHEN pic.country_id <> pir.country_id THEN 1 ELSE NULL END) )
+  / COUNT(*), 1) AS international_calls_pct
+FROM phone_calls AS pc
+LEFT JOIN phone_info AS pic
+  ON pc.caller_id = pic.caller_id
+LEFT JOIN phone_info AS pir
+  ON pc.receiver_id = pir.caller_id;
 
 -- Q40.
+/*
+Your team at JPMorgan Chase is soon launching a new credit card. You are asked to estimate how many cards you'll issue in the first month.
+
+Before you can answer this question, you want to first get some perspective on how well new credit card launches typically do in their first month.
+
+Write a query that outputs the name of the credit card, and how many cards were issued in its launch month. 
+The launch month is the earliest record in the monthly_cards_issued table for a given card. Order the results starting from the biggest issued amount.
+
+monthly_cards_issued(issue_year, issue_month, card_name, issued_amount)
+*/
+WITH card_launch AS (
+SELECT 
+  card_name,
+  issued_amount,
+  CAST(CONCAT_WS('-', issue_year, issue_month, 1)) AS issue_date,
+  MIN(CAST(CONCAT_WS('-', issue_year, issue_month, 1))) OVER (PARTITION BY card_name) AS launch_date
+FROM monthly_cards_issued
+)
+SELECT card_name, issued_amount
+FROM card_launch
+WHERE issue_date = launch_date
+ORDER BY issued_amount DESC;
